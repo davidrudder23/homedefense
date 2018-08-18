86d0e52b207a6ccbc2a391358371fbc83a19ae38
Dave Rudder <drig@noses.org>
2018-08-17 18:59:29 -0600

    She's not only my least favorite SG1 character.  She's also the new database for homedefense

---
+++ src/main/java/org/noses/homedefense/HomedefenseConfig.java
@@ -0,0 +1,48 @@
+package org.noses.homedefense;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = { "org.noses.homedefense.maps" })
+public class HomedefenseConfig extends AbstractCassandraConfiguration {
+    public static final String KEYSPACE = "dev";
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+
+        return Arrays.asList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return KEYSPACE;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[]{"org.noses.homedefense"};
+    }
+
+}
---
+++ src/main/java/org/noses/homedefense/maps/NodeRepository.java
@@ -0,0 +1,7 @@
+package org.noses.homedefense.maps;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface NodeRepository extends CrudRepository<Node, Long> {
+
+}
